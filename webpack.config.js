42549022ec7ddb8c3d0eeb056dba1d1851330995
simon-verhoeven <simonaverhoeven@gmail.com>
2021-10-18 13:46:36 +0800

    simplify wp config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,75 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/no-extraneous-dependencies */
-const { merge } = require('webpack-merge');
+
+/* eslint-disable class-methods-use-this */
 const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-const rootConfig = require(path.join(__dirname, './webpack.config.build.js'));
+const mode = process.env.NODE_ENV || 'development';
 
-const output = merge(rootConfig(__dirname), {
-  resolve: {
-    // workaround for webpack https://github.com/jaydenseric/graphql-upload/issues/258#issuecomment-897904695
-    mainFields: ['main'],
-  },
-});
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = output;
+module.exports = () => {
+  const SOURCE_DIR = path.join(__dirname, './src/');
+  const TARGET_DIR = path.join(__dirname, './dist/');
+
+  const filename = 'index.js';
+  const entry = path.join(SOURCE_DIR, 'index.ts');
+
+  const plugins = [new ForkTsCheckerWebpackPlugin()];
+
+  return {
+    node: {
+      global: false,
+      __filename: false,
+      __dirname: false,
+    },
+    mode,
+    devtool: 'source-map',
+    entry,
+    target: 'node',
+    resolve: {
+      extensions: ['.mjs', '.ts', '.js'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          /* options: see below */
+        }),
+      ],
+    },
+    output: {
+      libraryTarget: 'commonjs2',
+      path: TARGET_DIR,
+      filename,
+    },
+    //   externals: [],
+    module: {
+      rules: [
+        {
+          type: 'javascript/auto',
+          test: /\.mjs$/,
+          // resolve: {
+          //   fullySpecified: false,
+          // },
+        },
+        {
+          test: /.tsx?$/,
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: path.join(__dirname, mode === 'production' ? 'tsconfig.build.json' : 'tsconfig.json'),
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins,
+    externals: [{ 'aws-sdk': 'commonjs aws-sdk', y18n: 'commonjs2 y18n', 'yargs-parser': 'commonjs2 yargs-parser' }],
+    optimization: {
+      minimize: false,
+      //   Turned this off because it apollo server throws error, schema must contain uniquely named types but contains multiple types named "XXX"
+    },
+  };
+};
